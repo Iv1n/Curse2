5f28234eaaae6e67b561984803ed2a950dabd664
Iv1n <90642026+Iv1n@users.noreply.github.com>
2022-10-24 20:34:56 +0300

    Delete Buyer.java

--- 1/untitled/src/Buyer.java
+++
@@ -1,7 +0,0 @@
-public class Buyer extends Person {
-private String inPleb;
-
-    public Buyer(long id, String name, String birth, String gender) {
-        super(id, name, birth, gender);
-    }
-}
