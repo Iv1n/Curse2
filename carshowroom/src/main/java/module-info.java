9a0ee22532f217f89ff05101d2fe4c67cd42292e
Iv1n <90642026+Iv1n@users.noreply.github.com>
2023-02-24 15:38:14 +0300

    Delete carshowroom directory

--- carshowroom/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.carshowroom {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.carshowroom to javafx.fxml;
-    exports com.example.carshowroom;
-}
